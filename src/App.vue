005dc8dcceece1dbde3ba8631117a00b08dfe7b9
Gedson <gfaria76@users.noreply.github.com>
2021-04-22 18:16:27 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <playground></playground>
+      <v-text-field label="Teste"></v-text-field>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Playground from "./components/Playground";
+
+export default {
+  name: "App",
+  components: {
+    Playground
+  }
+};
+</script>
+
